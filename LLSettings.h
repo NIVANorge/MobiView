31878ae43aa02b39b39137a44820f109fbd41d2d
Maginor84 <magnus.dahler.norling@gmail.com>
2021-08-30 15:29:08 +0200

    Add missing file

---
+++ LLSettings.h
@@ -0,0 +1,8 @@
+
+//NOTE: The number is the amount of error parameters required (per target) when computing this
+//statistic.
+
+SET_LL_SETTING(Normal, "Normal", 1)
+SET_LL_SETTING(NormalHet1, "Normal hetereoskedastic (1)", 1)
+SET_LL_SETTING(WLS, "WLS", 2)
+SET_LL_SETTING(WLSAR1, "WLS-AR1", 3)
