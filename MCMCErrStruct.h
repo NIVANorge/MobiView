421e7e7f663bc7496d300eeab035524c50fd7d8c
Maginor84 <magnus.dahler.norling@gmail.com>
2021-09-30 13:14:08 +0200

    Add new files

---
+++ MCMCErrStruct.h
@@ -0,0 +1,19 @@
+#ifndef _MobiView_MCMCErrStruct_h_
+#define _MobiView_MCMCErrStruct_h_
+
+#include <vector>
+#include <random>
+
+#define SET_LL_SETTING(Handle, Name, NumErr) MCMCError_##Handle,
+enum mcmc_error_structure
+{
+	MCMCError_Unknown = -1,
+	#include "LLSettings.h"
+};
+#undef SET_LL_SETTING
+
+void AddRandomError(double* Series, size_t Timesteps, const std::vector<double> &ErrParam, mcmc_error_structure ErrStruct, std::mt19937_64 &Generator);
+void ComputeStandardizedResiduals(double *Obs, double *Sim, size_t Timesteps, const std::vector<double> &ErrParam, mcmc_error_structure ErrStruct, std::vector<double> &ResidualsOut);
+double ComputeLLValue(double *Obs, double *Sim, size_t Timesteps, const std::vector<double> &ErrParam, mcmc_error_structure ErrStruct);
+
+#endif
